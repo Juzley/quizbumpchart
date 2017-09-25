fc4b13f4c5f96606f6e134ef64b9ce1f910e1bdb
Juzley <juzley@gmail.com>
2017-09-25 23:13:00 +0100

    Colours, numbers

--- bumpchart.js
+++ bumpchart.js
@@ -14,7 +14,7 @@ BumpChart = (function ($) {
             position,
             numRounds = 3,
             rounds = [],
-            teamPositions = {},
+            teamInfo = {},
             sortFn;
 
         // Clear previous graphs
@@ -59,43 +59,70 @@ BumpChart = (function ($) {
             // Sort it to so that the best team is first.
             roundScores.sort(sortFn);
 
-            // Add the results into the teamPositions map - the index in the
+            // Add the results into the teamInfo map - the index in the
             // roundScores array is the position for that round.
             for (j = 0; j < roundScores.length; j += 1) {
                 teamName = roundScores[j][0];
+                score = roundScores[j][1];
                 position = j;
 
-                if (!teamPositions.hasOwnProperty(teamName)) {
-                    teamPositions[teamName] = [position];
+                if (!teamInfo.hasOwnProperty(teamName)) {
+                    teamInfo[teamName] = {
+                        positions: [position],
+                        scores: [score]
+                    };
                 } else {
-                    teamPositions[teamName].push(position);
+                    teamInfo[teamName].positions.push(position);
+                    teamInfo[teamName].scores.push(score);
                 }
             }
         }
 
-        return teamPositions;
+        return teamInfo;
     }
 
-    function drawChart(teamPositions) {
+    function teamColour(colourIndex) {
+        var colours = [
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "violet"
+        ];
+
+        return colours[colourIndex % colours.length];
+    }
+
+    function drawChart(teamInfo) {
         var columnWidth,
+            columnsStart,
             rowHeight = 50,
             teamName,
             teamNameWidth,
             teamNameHeight = 30,
             maxTeamNameWidth = 0,
             positions,
+            scores,
+            scoreString,
+            scoreWidth,
+            scoreHeight = 20,
             ctx,
+            colourIndex,
+            colourMap = {},
+            padding = 20,
             i;
 
         ctx = $("#graph")[0].getContext("2d");
-        ctx.canvas.height = rowHeight * Object.keys(teamPositions).length;
+        ctx.canvas.height = rowHeight * Object.keys(teamInfo).length;
         ctx.canvas.width = $(window).width() * 0.95;
 
         ctx.font = teamNameHeight.toString() + "pt Arial";
 
         // Find the width of the teamname columns
-        for (teamName in teamPositions) {
-            if (teamPositions.hasOwnProperty(teamName)) {
+        for (teamName in teamInfo) {
+            if (teamInfo.hasOwnProperty(teamName)) {
                 teamNameWidth = ctx.measureText(teamName).width;
                 if (teamNameWidth > maxTeamNameWidth) {
                     maxTeamNameWidth = teamNameWidth;
@@ -103,12 +130,22 @@ BumpChart = (function ($) {
             }
         }
 
+        // Set up team colours
+        colourIndex = 0;
+        for (teamName in teamInfo) {
+            if (teamInfo.hasOwnProperty(teamName)) {
+                colourMap[teamName] = teamColour(colourIndex);
+                colourIndex += 1;
+            }
+        }
+
         // Draw the teamnames
-        for (teamName in teamPositions) {
-            if (teamPositions.hasOwnProperty(teamName)) {
-                positions = teamPositions[teamName];
+        for (teamName in teamInfo) {
+            if (teamInfo.hasOwnProperty(teamName)) {
+                positions = teamInfo[teamName].positions;
                 teamNameWidth = ctx.measureText(teamName).width;
 
+                ctx.fillStyle = colourMap[teamName];
                 ctx.fillText(
                     teamName,
                     maxTeamNameWidth - teamNameWidth,
@@ -124,20 +161,45 @@ BumpChart = (function ($) {
         }
 
         // Draw the lines
-        columnWidth = (ctx.canvas.width - maxTeamNameWidth * 2) / 2;
-        for (teamName in teamPositions) {
-            if (teamPositions.hasOwnProperty(teamName)) {
-                positions = teamPositions[teamName];
+        columnWidth = (
+            (ctx.canvas.width - padding * 2 - maxTeamNameWidth * 2) / 2
+        );
+        columnsStart = maxTeamNameWidth + padding;
+        for (teamName in teamInfo) {
+            if (teamInfo.hasOwnProperty(teamName)) {
+                positions = teamInfo[teamName].positions;
 
                 ctx.beginPath();
-                ctx.moveTo(maxTeamNameWidth, (positions[0] + 0.5) * rowHeight);
+                ctx.moveTo(columnsStart, (positions[0] + 0.5) * rowHeight);
                 for (i = 1; i < positions.length; i += 1) {
                     ctx.lineTo(
-                        maxTeamNameWidth + columnWidth * i,
+                        columnsStart + columnWidth * i,
                         (positions[i] + 0.5) * rowHeight
                     );
                 }
+                ctx.strokeStyle = colourMap[teamName];
                 ctx.stroke();
+            }
+        }
+
+        // Draw scores over the lines
+        ctx.font = scoreHeight.toString() + "pt Arial";
+        for (teamName in teamInfo) {
+            if (teamInfo.hasOwnProperty(teamName)) {
+                positions = teamInfo[teamName].positions;
+                scores = teamInfo[teamName].scores;
+
+                ctx.fillStyle = colourMap[teamName];
+                for (i = 0; i < scores.length; i += 1) {
+                    scoreString = scores[i].toString();
+                    scoreWidth = ctx.measureText(scoreString).width;
+
+                    ctx.fillText(
+                        scoreString,
+                        columnsStart + columnWidth * i - scoreWidth / 2,
+                        (positions[i] + 0.5) * rowHeight + scoreHeight / 2
+                    );
+                }
             }
         }
     }
